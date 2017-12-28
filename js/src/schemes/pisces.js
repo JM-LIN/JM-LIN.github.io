2c5d4465a78215040b517062e98bda886e105297
JM.Lin <IamJM.Lin@gmail.com>
2017-12-28 17:35:03 +0800

    Site updated: 2017-12-28 17:34:54

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Fri Dec 22 2017 21:00:48 GMT+0800 (中国标准时间)
+// build time:Thu Dec 28 2017 17:34:48 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
